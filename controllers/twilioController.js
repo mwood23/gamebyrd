f1e26b1315fa6556d0486bd5319afd43527e74be
Marcus Wood <marcuswood230@gmail.com>
2016-03-17 02:19:42 -0600

    twilio

--- controllers/twilioController.js
+++ controllers/twilioController.js
@@ -2,33 +2,26 @@ var config = require('../config.js')
 var twilio = require('twilio')(config.account_sid, config.auth_token)
 
 function tight (req, res) {
+	var twiml = new twilio.TwimlResponse();
 
-	  var body = '';
+	console.log('made it to twilio', req.body, req.body.Body)
+	var x = req.body.Body.toLowerCase()
+	console.log(x)
+	
 
-	  req.on('data', function(data) {
-	    body += data;
-	  });
+	console.log(x, twiml)
 
-	  req.on('end', function() {
-	    //Create TwiML response
+	if (x == "tight") {		
+			console.log('inside tight')
+		    twiml.message("Oh it be ultra tight. About as tight as this quote from Kanye West, -- Super inspired by my visit to Ikea today , really amazing company… my mind is racing with the possibilities…");
+	} else {
+			console.log('inside else')
+		    twiml.message("Sorry, I don't work dat good yet");
 
-			console.log('made it to twilio', req.body, req.body.Body, body)
-			var twiml = new twilio.TwimlResponse();
+	} 		
 
-			console.log(x, twiml)
-
-			if (x == "tight") {		
-					console.log('inside tight')
-				    twiml.message("Oh it be ultra tight. About as tight as this quote from Kanye West, -- Super inspired by my visit to Ikea today , really amazing company… my mind is racing with the possibilities…");
-			} else {
-					console.log('inside else')
-				    twiml.message("Sorry, I don't work dat good yet");
-
-			} 		
-
-					res.writeHead(200, {'Content-Type': 'text/xml'});
-				    res.end(twiml.toString());
-})
+			res.writeHead(200, {'Content-Type': 'text/xml'});
+		    res.end(twiml.toString());
 }
 
 module.exports = {
