9122c16cae24446d2210c9a9c91b090f63cde015
Marcus Wood <marcuswood230@gmail.com>
2016-03-19 01:10:58 -0600

    twilio

--- controllers/twilioController.js
+++ controllers/twilioController.js
@@ -1,29 +1,29 @@
 var config = require('../config.js')
-var twilio = require('twilio')(config.interface_sid, config.auth_token)
+var twilio = require('twilio')(config.account_sid, config.auth_token)
 
 function tight (req, res) {
 	
 
 	console.log('made it to twilio', req.body, req.body.Body)
-	var twiml = new twilio.TwimlResponse();
+	var resp = new twilio.TwimlResponse();
 	var x = req.body.Body.toLowerCase()
 	console.log(x)
 	
 
-	console.log(x, twiml)
+	console.log(x, resp)
 
 	if (x == "tight") {		
 			console.log('inside tight')
 			
-		    twiml.message("Oh it be ultra tight. About as tight as this quote from Kanye West, -- Super inspired by my visit to Ikea today , really amazing company… my mind is racing with the possibilities…");
+		    resp.message("Oh it be ultra tight. About as tight as this quote from Kanye West, -- Super inspired by my visit to Ikea today , really amazing company… my mind is racing with the possibilities…");
 	} else {
 			console.log('inside else')
-		    twiml.message("Sorry, I don't work dat good yet");
+		    resp.message("Sorry, I don't work dat good yet");
 
 	} 		
 
 			res.writeHead(200, {'Content-Type': 'text/xml'});
-		    res.end(twiml.toString());
+		    res.end(resp.toString());
 }
 
 module.exports = {
